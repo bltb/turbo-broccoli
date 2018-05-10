783f3585baceca51788417725d93b11768f3dae2
Benjamin Lee <benjamin.lee@realthought.net>
2018-05-10 20:23:13 +1000

    Force tty (for Jenkins).

--- test_1.py
+++ test_1.py
@@ -2,8 +2,9 @@
 
 import coloredlogs, logging
 
+
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG', logger=logger)
+coloredlogs.install(level='DEBUG', logger=logger, isatty=True)
 
 logger.debug("this is a debugging message")
 logger.info("this is an informational message")
